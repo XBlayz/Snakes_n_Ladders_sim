9cac2b869f5219d18162ab2e408f69e4352223cc
XBlayz <stefano.scarcelli15@gmail.com>
2024-07-06 15:10:03 +0200

    Update Deck.java
    Implemented cards queue, drawCard() and discardCard(Card card) method

--- app/src/main/java/snakes_n_ladders_sim/simulation/entities/Deck.java
+++ app/src/main/java/snakes_n_ladders_sim/simulation/entities/Deck.java
@@ -1,21 +1,27 @@
 package snakes_n_ladders_sim.simulation.entities;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 import snakes_n_ladders_sim.simulation.entities.cards.Card;
 
 public class Deck {
+    private Queue<Card> cards = new LinkedList<>();
 
     protected Deck() {
         //TODO Auto-generated constructor stub
     }
 
     public Card drawCard() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'drawCard'");
+        Card drawnCard = cards.poll();
+        if (drawnCard.isInstant) { // If card is instant, discard it immediately
+            cards.add(drawnCard);
+        }
+        return drawnCard;
     }
 
-    public void discardCard(Card doNotStop) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'discardCard'");
+    public void discardCard(Card card) {
+        cards.add(card);
     }
 
 }
