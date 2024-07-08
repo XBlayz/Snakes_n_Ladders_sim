eabb7d4ba79c420b5fe25eae5f35699368f73599
XBlayz <stefano.scarcelli15@gmail.com>
2024-07-08 16:01:10 +0200

    Update Deck.java
    Implemented card shuffler constructor

--- app/src/main/java/snakes_n_ladders_sim/simulation/entities/Deck.java
+++ app/src/main/java/snakes_n_ladders_sim/simulation/entities/Deck.java
@@ -2,15 +2,32 @@ package snakes_n_ladders_sim.simulation.entities;
 
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
 import snakes_n_ladders_sim.simulation.entities.cards.Card;
+import snakes_n_ladders_sim.utility.*;
 
 public class Deck {
     // Cards collection
     private Queue<Card> cards = new LinkedList<>();
 
-    public Deck() {
-        //TODO
+    public Deck(List<Tuple<Card, Integer>> cardList) {
+        Random rng = new Random(System.currentTimeMillis()); // Initialize random number generator
+
+        while (!cardList.isEmpty()) {
+            Tuple<Card, Integer> cardAndN = cardList.get(rng.nextInt(cardList.size())); // Pick a random card
+
+            cards.add(cardAndN.getX()); // Add the card to the deck
+
+            if(cardAndN.getY() > 1){ // If the number of the card is greater than 1:
+                cardAndN.setY(cardAndN.getY() - 1); // Reduce the number of the card by 1
+            }else{
+                cardList.remove(cardAndN); // Else, remove the card from the list
+            }
+
+            // Repeat until the list is empty and all the cards have been added to the deck shuffled
+        }
     }
 
     public Card drawCard() {
