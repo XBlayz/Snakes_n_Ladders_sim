c93c76dafecab3754ad83b161c6b33b9cabc8beb
XBlayz <stefano.scarcelli15@gmail.com>
2024-07-10 13:09:30 +0200

    Update BoardBuildStrategy.java
    Import correction

--- app/src/main/java/snakes_n_ladders_sim/simulation/entities/board_build_strategy/BoardBuildStrategy.java
+++ app/src/main/java/snakes_n_ladders_sim/simulation/entities/board_build_strategy/BoardBuildStrategy.java
@@ -1,7 +1,6 @@
 package snakes_n_ladders_sim.simulation.entities.board_build_strategy;
 
 import snakes_n_ladders_sim.simulation.entities.cells.Cell;
-import snakes_n_ladders_sim.simulation.mediator.Mediator;
 
 public interface BoardBuildStrategy {
     Cell[][] buildBoard(int rows, int columns);
