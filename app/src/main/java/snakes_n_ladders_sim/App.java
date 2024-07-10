9097588277acd28311fb384b4f8750678fab989a
XBlayz <stefano.scarcelli15@gmail.com>
2024-07-10 16:43:58 +0200

    Update App.java
    Comment out javafx code

--- app/src/main/java/snakes_n_ladders_sim/App.java
+++ app/src/main/java/snakes_n_ladders_sim/App.java
@@ -10,17 +10,18 @@ import snakes_n_ladders_sim.simulation.entities.board_build_strategy.BoardBuildS
 import snakes_n_ladders_sim.simulation.entities.board_build_strategy.concrete_builder.RandomBoardBuilder;
 import snakes_n_ladders_sim.simulation.Match;
 
-public class App extends Application {
+//extends Application
+public class App {
 
-    @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+    // @Override
+    // public void start(Stage stage) {
+    //     String javaVersion = System.getProperty("java.version");
+    //     String javafxVersion = System.getProperty("javafx.version");
+    //     Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+    //     Scene scene = new Scene(new StackPane(l), 640, 480);
+    //     stage.setScene(scene);
+    //     stage.show();
+    // }
 
     public static void main(String[] args) throws InterruptedException {
         BoardBuildStrategy boardBuildStrategy = new RandomBoardBuilder(true, true, true);
